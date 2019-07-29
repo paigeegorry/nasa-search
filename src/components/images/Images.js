c8750dc3b6c7ba8669f44e6102d3aacf2e5672ba
Paige Gorry <paigeegorry@gmail.com>
2019-07-29 12:01:58 -0700

    added correct alt tag and got rid of console.log

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -3,12 +3,12 @@ import { useSelector } from 'react-redux';
 
 export default function Images() {
   const photos = useSelector(state => state);
-  console.log(photos[0]);
   const photoList = photos.map((photo, i) => {
     return (
-      <img src={photo.links[0].href} alt="hello" key={i} />
+      <img src={photo.links[0].href} alt={photo.data[0].title} key={i} />
     )
   });
+
   return (
     <>
     {photoList}
