f238b53f1efaf88c1a0174313d9ca4a6c2d30ae1
Paige Gorry <paigeegorry@gmail.com>
2019-08-02 11:41:11 -0700

    updated Keywords component and gitignore

--- src/components/Keywords.js
+++ src/components/Keywords.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPhotosApi } from '../services/nasaFetch';
+import { Button } from 'react-bootstrap';
 
 export default function Keywords() {
+  const dispatch = useDispatch();
   const keywords = useSelector(state => state.keywords);
-  const yolo = Object.entries(keywords).sort((a, b) => b[1]-a[1]);
+  const arrOfKeywords = Object.entries(keywords).sort((a, b) => b[1]-a[1]);
 
-  const listOfKeywords = yolo.map(ele => {
-    return (<li>
-
-          {ele[0]}
-          </li>)
+  const listOfKeywords = arrOfKeywords.map(keyword => {
+    return (
+      <li>
+        <Button onClick={() => dispatch({ type: 'FETCH_PHOTOS', payload: fetchPhotosApi(keyword[0]) })}>
+          {keyword[0]}
+        </Button>
+      </li>
+    )
   })
     return (
     <ul>
       {listOfKeywords.slice(0, 5)}
-
     </ul>
   )
 }
